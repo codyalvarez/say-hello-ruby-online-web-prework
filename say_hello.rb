4ebe62ba53b03605cf6efaad58d0ebc43961f039
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-02-28 21:21:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,3 @@
-def say_hello(name)
-puts  "Hello Kent Beck!"
-end
-
 def say_hello(arg="name")
   return arg
 end
-
-def ("Hello Kent Beck!") #=> "Hello Kent Beck!"
-def (name)
-say_hello("chris")
