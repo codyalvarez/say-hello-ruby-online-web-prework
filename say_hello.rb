d46adf34e71523ddd7be1e6b80156c07c66046c5
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-02-28 21:00:43 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,10 @@
 def say_hello(name)
 puts  "Hello Kent Beck!"
 end
+
+def myMethod(arg="Hello Kent Beck!")
+  return arg
+end
+
+myMethod("Hello Kent Beck!") #=> "hi"
+myMethod() #=> "hello world!"
